e5e7f7d0cb5e9afb0154ee49086dfbc97128cbd5
Brent <bcen0001@student.monash.edu>
2024-09-02 16:05:28 +1000

    Redesigned the custom toggle using offsets instead of weights

--- composeApp/src/commonMain/kotlin/com/brentcodes/campose/components/Toggles.kt
+++ composeApp/src/commonMain/kotlin/com/brentcodes/campose/components/Toggles.kt
@@ -2,6 +2,7 @@ package com.brentcodes.campose.components
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntOffsetAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -9,6 +10,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,11 +28,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
+import kotlin.math.roundToInt
 
 @Composable
 fun MainToggle(modifier: Modifier = Modifier) {
@@ -41,6 +49,39 @@ fun MainToggle(modifier: Modifier = Modifier) {
         Switch(checked = checked.value, onCheckedChange = { checked.value = !checked.value })
         CustomToggle(state = checked.value, onCheckedChange = {checked.value = !checked.value})
         CustomizedSwitch(checked = checked.value, onCheckedChange = {checked.value = !checked.value})
+        NewCustomToggle(state = checked.value, onCheckedChange = { checked.value = !checked.value })
+    }
+}
+
+@Composable
+fun NewCustomToggle(modifier: Modifier = Modifier, height: Int = 70, width: Int = 140, state: Boolean, onCheckedChange: () -> Unit = {}) {
+
+    val color by animateColorAsState(if (!state) Color.LightGray else Color(0xFF15d137))
+    val thumbColor by animateColorAsState(if (!state) Color.DarkGray else Color.White)
+    val border by animateColorAsState(if (!state) Color.DarkGray else Color.Transparent)
+
+    val sizePx = with(LocalDensity.current) { (width - height).toDp().toPx() }
+    val offset = animateIntOffsetAsState(IntOffset(x = if (!state) 0 else sizePx.roundToInt(), 0)).value
+
+    Row(
+        modifier = modifier
+            .height(height.dp)
+            .width(width.dp)
+            .clip(RoundedCornerShape(height.dp))
+            .background(color)
+            .border(2.dp, border, RoundedCornerShape(height.dp))
+            .clickable {
+                onCheckedChange()
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .offset { offset }
+                .size(height.dp)
+                .clip(RoundedCornerShape(50))
+                .background(thumbColor)
+        ) {}
     }
 }
 
