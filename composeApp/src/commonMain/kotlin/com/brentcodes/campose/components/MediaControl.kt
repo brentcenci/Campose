f3792aba1199889326d0899c24739eea05cc9eea
Brent <bcen0001@student.monash.edu>
2024-09-22 14:46:07 +1000

    Added a title and artist section to the Alt Media design

--- composeApp/src/commonMain/kotlin/com/brentcodes/campose/components/MediaControl.kt
+++ composeApp/src/commonMain/kotlin/com/brentcodes/campose/components/MediaControl.kt
@@ -1,10 +1,14 @@
 package com.brentcodes.campose.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Slider
@@ -19,9 +23,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import campose.composeapp.generated.resources.Res
 import campose.composeapp.generated.resources.favorite_filled
 import campose.composeapp.generated.resources.favorite_outline
@@ -84,24 +92,32 @@ fun BasicMusicControls(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun AlternativeMusicControls(modifier: Modifier = Modifier, duration: Int = 128) {
+fun AlternativeMusicControls(modifier: Modifier = Modifier, duration: Int = 128, songTitle: String = "Song Title", artist: String = "Artist Name ft. Artist") {
     var sliderValue by remember { mutableStateOf(0f) }
     var isPlaying by remember { mutableStateOf(false) }
     var shuffle by remember { mutableStateOf(0) }
     var inPlaylist by remember { mutableStateOf(false) }
 
     Column(modifier = modifier.width(300.dp)) {
-        Column {
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Text(text = "${duration.times(sliderValue).roundToInt().floorDiv(60)}:${duration.times(sliderValue).roundToInt().rem(60).toString().padStart(2, '0')}")
-                Spacer(modifier = Modifier.weight(1f))
-                Text(text = "${duration.floorDiv(60)}:${(duration%60).toString().padStart(2, '0')}")            }
-            Slider(
-                value = sliderValue,
-                onValueChange = { sliderValue = it },
-                modifier = Modifier.fillMaxWidth(),
-                colors = SliderDefaults.colors(thumbColor = Color.Black, activeTrackColor = Color(0xFF5f6368))
-            )
+
+        Box(modifier = Modifier.fillMaxWidth().height(200.dp), contentAlignment = Alignment.Center) {
+            Column() {
+                Text(text = songTitle, fontSize = 40.sp, fontWeight = FontWeight.Bold, maxLines = 1, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
+                Text(text = artist, maxLines = 1, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
+            }
+
+        }
+
+        Slider(
+            value = sliderValue,
+            onValueChange = { sliderValue = it },
+            modifier = Modifier.fillMaxWidth(),
+            colors = SliderDefaults.colors(thumbColor = Color.Black, activeTrackColor = Color(0xFF5f6368))
+        )
+        Row(modifier = Modifier.fillMaxWidth().offset(y = (-12).dp), verticalAlignment = Alignment.Top) {
+            Text(text = "${duration.times(sliderValue).roundToInt().floorDiv(60)}:${duration.times(sliderValue).roundToInt().rem(60).toString().padStart(2, '0')}", fontSize = 12.sp)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = "${duration.floorDiv(60)}:${(duration%60).toString().padStart(2, '0')}", fontSize = 12.sp)
         }
         Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
             IconButton(onClick = { shuffle = (shuffle + 1) % 3 }) {
